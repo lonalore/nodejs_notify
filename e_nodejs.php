03a8013739d8df95b8a472682ab1c37d1c49716e
Lóna Lore <juhasz.sandor@lonalore.hu>
2015-02-15 19:43:57 +0100

    implements e_nodejs

---
+++ e_nodejs.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @file
+ *
+ */
+
+/**
+ * Class nodejs_notify_nodejs.
+ */
+class nodejs_notify_nodejs {
+
+	/**
+	 * Node.js Javascript handlers.
+	 *
+	 * @return array
+	 * 	The list of JavaScript handler files.
+	 */
+	public function jsHandlers() {
+		return array(
+			'js/nodejs_notify.js',
+		);
+	}
+
+
+	/**
+	 * Node.js message handlers.
+	 *
+	 * @return array
+	 * 	The list of message callbacks.
+	 */
+	public function msgHandlers() {
+		return array();
+	}
+
+
+	/**
+	 * Node.js user channels.
+	 *
+	 * @return array
+	 * 	The list of user channels.
+	 */
+	public function userChannels() {
+		return array();
+	}
+
+
+	/**
+	 * Node.js user presence list.
+	 *
+	 * @param $account
+	 *
+	 * @return array
+	 * 	List of users who can see presence notifications about me.
+	 */
+	public function userPresenceList($account) {
+		return array();
+	}
+
+}
